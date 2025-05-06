05115044492a53c944bc42ed302ed7ca665b3635
Matthew <mmarsteiner@gmail.com>
2025-05-05 22:21:12 -0400

    Create static matrix generation methods

--- src/Graphics/Mat4.cpp
+++ src/Graphics/Mat4.cpp
@@ -20,4 +20,69 @@ void Mat4::Multiply(const Mat4& rhs, Mat4& out) const {
     }
 }
 
+void Mat4::CreateIdentityMatrix(Mat4& out) {
+    for (int row = 0; row < 4; ++row) {
+        for (int col = 0; col < 4; ++col) {
+            if (row == col) {
+                out.data[row * 4 + col] = 1;
+            } else {
+                out.data[row * 4 + col] = 0;
+            }
+        }
+    }
+}
+
+void Mat4::CreateTranslationMatrix(Mat4& out, double tx, double ty, double tz) {
+    CreateIdentityMatrix(out);
+    out.data[3] = tx;
+    out.data[7] = ty;
+    out.data[11] = tz;
+}
+
+void Mat4::CreateRotationMatrixX(Mat4& out, double theta) {
+    CreateIdentityMatrix(out);
+    out.data[5] = cos(theta);
+    out.data[6] = -sin(theta);
+    out.data[9] = sin(theta);
+    out.data[10] = cos(theta);
+}
+
+void Mat4::CreateRotationMatrixY(Mat4& out, double theta) {
+    CreateIdentityMatrix(out);
+    out.data[0] = cos(theta);
+    out.data[2] = sin(theta);
+    out.data[8] = -sin(theta);
+    out.data[10] = cos(theta);
+}
+
+void Mat4::CreateRotationMatrixZ(Mat4& out, double theta) {
+    CreateIdentityMatrix(out);
+    out.data[0] = cos(theta);
+    out.data[1] = -sin(theta);
+    out.data[4] = sin(theta);
+    out.data[5] = cos(theta);
+}
+
+void Mat4::CreatePerspectiveMatrix(Mat4& out, double near, double far, double right, double left, double top, double bottom) {
+    double invWidth = 1 / (right - left);
+    double invHeight = 1 / (top - bottom);
+    double invDepth = 1 / (far - near);
+    out.data[0] = 2 * near * invWidth;         // [0][0]
+    out.data[1] = 0;                           // [0][1]
+    out.data[2] = (right + left) * invWidth;   // [0][2]
+    out.data[3] = 0;                           // [0][3]
+    out.data[4] = 0;                           // [1][0]
+    out.data[5] = 2 * near * invHeight;        // [1][1]
+    out.data[6] = (top + bottom) * invHeight;  // [1][2]
+    out.data[7] = 0;                           // [1][3]
+    out.data[8] = 0;                           // [2][0]
+    out.data[9] = 0;                           // [2][1]
+    out.data[10] = -(far + near) * invDepth;   // [2][2]
+    out.data[11] = -2 * far * near * invDepth; // [2][3]
+    out.data[12] = 0;                          // [3][0]
+    out.data[13] = 0;                          // [3][1]
+    out.data[14] = -1;                         // [3][2]
+    out.data[15] = 0;                          // [3][3]
+}
+
 }  // namespace g3d
--- src/Graphics/Mat4.h
+++ src/Graphics/Mat4.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <cstring>
+#include <cmath>
+
 #include "Vec4.h"
 
 namespace g3d {
@@ -11,6 +13,13 @@ class Mat4 {
     public:
     void Transform(const Vec4& in, Vec4& out) const;
     void Multiply(const Mat4& rhs, Mat4& out) const;
+
+    static void CreateIdentityMatrix(Mat4& out);
+    static void CreateTranslationMatrix(Mat4& out, double tx, double ty, double tz);
+    static void CreateRotationMatrixX(Mat4& out, double theta);
+    static void CreateRotationMatrixY(Mat4& out, double theta);
+    static void CreateRotationMatrixZ(Mat4& out, double theta);
+    static void CreatePerspectiveMatrix(Mat4& out, double near, double far, double right, double left, double top, double bottom);
 };
 
 }
