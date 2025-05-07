976b386d08831bd50a4c08b13a7045af6cd5cf18
Matthew <mmarsteiner@gmail.com>
2025-05-07 18:47:11 -0400

    Make Vec4 header only and store data in array for faster access

--- src/Graphics/Vec4.h
+++ src/Graphics/Vec4.h
@@ -5,14 +5,25 @@
 
 namespace g3d {
 
-struct Vec4 {
-    double x;
-    double y;
-    double z;
-    double w;
-    Vec4();
-    Vec4(double x, double y, double z, double w);
-    double& operator[](int idx);
+class Vec4 {
+    double data[4] {};
+public:
+    double& x;
+    double& y;
+    double& z;
+    double& w;
+
+    Vec4() : Vec4(0, 0, 0, 0) {}
+    Vec4(double x, double y, double z, double w) : x{data[0]}, y{data[1]}, z{data[2]}, w{data[3]} {
+        data[0] = x;
+        data[1] = y;
+        data[2] = z;
+        data[3] = w;
+    }
+
+    double& operator[](int idx) { 
+        return data[idx];
+    }
 };
 
 }
